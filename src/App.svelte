3aec54d8abc7d7084b68ab52a795ecc3e70e6ed4
hramtsov13 <hramtsov13@mail.ru>
2020-08-01 15:59:05 +0500

    100x100 box

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  let name = "Svelte";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  :global(h1) {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  .playground {
+    margin-left: auto;
+    margin-right: auto;
+    width: 100px;
+    height: 100px;
+    background-color: blanchedalmond;
+    opacity: 0.5;
+    border: 1px solid #000;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>{name}</h1>
+
+  <div class="playground" />
+
+</main>
